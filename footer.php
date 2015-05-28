45d940b0b997ffff947555bc9dfd9d2398c06fc8
Amy Tang <amytangcodes@gmail.com>
2015-05-28 12:42:51 -0400

    initial commit

---
+++ footer.php
@@ -0,0 +1,32 @@
+<footer>
+ <div class="container">
+     <article class="footer leftSide">
+          <i class="fa fa-phone"></i>
+          <i class="fa fa-envelope-o"></i>
+          <i class="fa fa-map-marker"></i>
+     </article>
+     <article class="footer middle">
+         <p>&copy; Monatang <?php echo date('Y'); ?></p>
+     </article>
+     <article class="footer rightSide">
+         <i class="fa fa-facebook-square"></i>
+         <i class="fa fa-twitter-square"></i>
+         <i class="fa fa-instagram"></i>
+         <i class="fa fa-pinterest-square"></i>
+     </article>
+ </div>
+</footer>
+
+
+
+<script>
+/* Google Analytics! */
+ var _gaq=[["_setAccount","UA-XXXXX-X"],["_trackPageview"]]; // Change UA-XXXXX-X to be your site's ID
+ (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=1;
+ g.src=("https:"==location.protocol?"//ssl":"//www")+".google-analytics.com/ga.js";
+ s.parentNode.insertBefore(g,s)}(document,"script"));
+</script>
+
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ front-page.php
@@ -0,0 +1,26 @@
+<!--how to make dynamic?-->
+<?php get_header(); ?>
+<section class="headerBackground">
+ <h2>Just Be.</h2> <!--make dynamic-->
+ <button class="getStarted">Get Started</button>
+</section>
+
+<div class="container">
+   <section class="frontPageContent">
+     
+     <div class="quickOverview">
+       <article class="overviewLeft">
+         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid in dolore eius dolor, amet suscipit animi provident non. Ipsam possimus sapiente porro culpa, laudantium nisi similique deserunt corrupti, voluptas sed.</p>
+       </article>
+       <article class="overviewMiddle">
+         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla labore libero explicabo nihil, soluta earum, recusandae necessitatibus. Optio voluptatibus earum velit repellat non, fugit necessitatibus. Dolores autem sint harum, necessitatibus.</p>
+       </article>
+       <article class="overviewRight">
+         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea magni praesentium alias molestias, esse numquam provident. In dolore, non reprehenderit ullam laboriosam vel aperiam iste ut ipsam. Mollitia, deserunt illo.</p>
+       </article>
+     </div>
+   
+   </section>
+</div>
+
+<?php get_footer(); ?>
