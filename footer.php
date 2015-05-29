f04cd47d9541dd5017dceb31a8b3fd9d077b5478
Amanda Monaco <amandagmonaco@gmail.com>
2015-05-29 14:07:04 -0400

    updated styles

--- footer.php
+++ footer.php
@@ -1,22 +1,30 @@
 <footer>
-
- <div class="container">
-     <article class="footer leftSide">
-          <i class="fa fa-phone"></i>
-          <i class="fa fa-envelope-o"></i>
-          <i class="fa fa-map-marker"></i>
-     </article>
-     <article class="footer middle">
-     	<img class="lotusBottom" src="<?php bloginfo('template_directory') ?>/img/lotus2.svg" alt="">
-         <p>&copy; Monatang <?php echo date('Y'); ?></p>
-     </article>
-     <article class="footer rightSide">
-         <i class="fa fa-facebook-square"></i>
-         <i class="fa fa-twitter-square"></i>
-         <i class="fa fa-instagram"></i>
-         <i class="fa fa-pinterest-square"></i>
-     </article>
- </div>
+	 <div class="container flexContainer">
+	     <article class="footer leftSide">
+	          <div class="phone">
+	          	<i class="fa fa-phone"></i>
+	          	<p>555-555-5555</p>
+	          </div>
+	          <div class="email">
+	          	<i class="fa fa-envelope-o"></i>
+	          	<p>yourname@youremail.com</p>
+	          </div>
+	          <div class="address">
+	          	<i class="fa fa-map-marker"></i>
+	          	<p>123 Main Street, Toronto, Canada</p>
+	          </div>
+	     </article>
+	     <article class="footer middle">
+	     	<img class="lotusBottom" src="<?php bloginfo('template_directory') ?>/img/lotus.svg" alt="">
+	         <p>&copy; Monatang <?php echo date('Y'); ?></p>
+	     </article>
+	     <article class="footer rightSide">
+	         <a href="#" class="facebook"><i class="fa fa-facebook-square"></i></a>
+	         <a href="#" class="twitter"><i class="fa fa-twitter-square"></i></a>
+	         <a href="#" class="instagram"><i class="fa fa-instagram"></i></a>
+	         <a href="#" class="pinterest"><i class="fa fa-pinterest-square"></i></a>
+	     </article>
+	 </div>
 </footer>
 
 <script>
