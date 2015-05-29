d6875edb06312c75364f46e73d3981a1c76d6a89
Amy Tang <amytangcodes@gmail.com>
2015-05-29 15:18:47 -0400

    updated sidebar

--- single.php
+++ single.php
@@ -4,9 +4,10 @@
   <div class="container">
 
     <div class="content">
+      
       <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
           
-        <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+        <div class="postSection" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
           
           <div class="entry-meta">
 
